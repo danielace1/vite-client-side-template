a1bb3a844ac06554788e2620df9f684da6740506
danielace1 <danielsudharsan1805@gmail.com>
2024-03-08 18:26:08 +0530

    fix: commit-mistakes

--- src/components/TheNavbar.jsx
+++ src/components/TheNavbar.jsx
@@ -1,14 +1,29 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const TheNavbar = () => {
-    return <div className="bg-[#7B2869] p-2  sm:px-10 sm:py-5 flex items-center justify-between">
-        <h4 className="font-semibold sm:text-xl text-sm text-white">Swetha Ramesh</h4>
-        <ul className="flex space-x-4">
-            <li className="text-white sm:text-base text-sm hover:text-slate-400"><Link to={'./'}>Home</Link></li>
-            <li className="text-white sm:text-base text-sm hover:text-slate-400"><Link to={"./about"}>About</Link></li>
-            <li className="text-white sm:text-base text-sm hover:text-slate-400" ><Link to={"./contact"}>Contact</Link></li>
-        </ul>
-    </div>
-}
+  return (
+    <nav className="bg-zinc-800 p-2  sm:px-10 sm:py-5 flex items-center justify-between">
+      <h4 className="font-semibold sm:text-xl text-sm text-white">Sudharsan</h4>
 
-export default TheNavbar
+      <ul className="flex space-x-4">
+        <li className="text-white sm:text-base text-sm hover:text-slate-400">
+          <NavLink to={"/"} className="active-nav" aria-current="page">
+            Home
+          </NavLink>
+        </li>
+        <li className="text-white sm:text-base text-sm hover:text-slate-400">
+          <NavLink to={"/about"} className="active-nav" aria-current="page">
+            About
+          </NavLink>
+        </li>
+        <li className="text-white sm:text-base text-sm hover:text-slate-400">
+          <NavLink to={"/contact"} className="active-nav" aria-current="page">
+            Contact
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default TheNavbar;
--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,11 +1,9 @@
-import React from 'react'
-
 const AboutPage = () => {
   return (
-    <div>
-      <h1>about page</h1>
+    <div className="min-h-screen bg-gray-100">
+      <h1 className="text-center text-4xl">About page</h1>
     </div>
-  )
-}
+  );
+};
 
-export default AboutPage
+export default AboutPage;
