d58e5a51c8d04f941548dd71eb66a502187432f1
danielace1 <danielsudharsan1805@gmail.com>
2024-03-08 18:18:48 +0530

    Revert "init"
    This commit is a mistake

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,9 @@
+const AboutPage = () => {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <h1 className="text-center text-4xl">About page</h1>
+    </div>
+  );
+};
+
+export default AboutPage;
---
+++ src/pages/ContactPage.jsx
@@ -0,0 +1,9 @@
+const ContactPage = () => {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <h1 className="text-center text-4xl">Contact page</h1>
+    </div>
+  );
+};
+
+export default ContactPage;
